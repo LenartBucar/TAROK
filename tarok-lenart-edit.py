7ea6801707de8838ea4ede2c85137cb777a8cfb0
gregorspv <gregorgajic.spv@gmail.com>
2017-04-21 19:34:33 +0200

    brez tqnekaj, ker jst tega nimam hahaha

--- tarok-lenart-edit.py
+++ tarok-lenart-edit.py
@@ -26,7 +26,7 @@ def main():
         if check_legal(divide_cards(deck)):
             valid += 1
         if x%100 == 0:
-            t.set_postfix(valid=valid*100/x, invalid=(x-valid)*100/x)
+            # t.set_postfix(valid=valid*100/x, invalid=(x-valid)*100/x)
     print("\n\nKONEC")
     print("Legalnih je bilo {}% rok.".format((valid*100/args.cycles)))
     print("Nelegalnih je bilo {}% rok.".format((args.cycles-valid)*100/args.cycles))
